06b20ae9d8366556a671acc0593675fc3450a78f
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-01-25 14:35:25 -0300

    Finishing app and implements focus in elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export function Header() {
       <Dialog.Root>
         <Dialog.Trigger
           type="button"
-          className="py-4 px-6 border-2 border-violet-500 rounded-lg font-semibold flex items-center gap-3 hover:border-violet-300 focus:outline-none focus:ring-2 focus:ring-violet-400 focus:rounded"
+          className="py-4 px-6 border-2 border-violet-500 rounded-lg font-semibold flex items-center gap-3 transition-colors hover:border-violet-300 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background"
         >
           <Plus size={20} className="text-violet-500" />
           Novo hábito
@@ -23,7 +23,7 @@ export function Header() {
           <Dialog.Overlay className="w-screen h-screen bg-black/70 fixed inset-0" />
 
           <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:rounded">
+            <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-lg hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-500">
               <X size={24} aria-label="Fechar" />
             </Dialog.Close>
 
--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -82,11 +82,11 @@ export function NewHabitForm() {
             return (
               <Checkbox.Root
                 key={weekDay}
-                className="flex items-center gap-3 group"
-                checked={weekDays.includes(index)}
                 onCheckedChange={() => handleToggleWeekDay(index)}
+                checked={weekDays.includes(index)}
+                className="flex items-center gap-3 group focus:outline-none"
               >
-                <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 transition-colors group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-zinc-900">
                   <Checkbox.Indicator>
                     <Check size={20} className="text-white" />
                   </Checkbox.Indicator>
@@ -100,7 +100,7 @@ export function NewHabitForm() {
 
         <button
           type="submit"
-          className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-green-400 focus:rounded"
+          className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 transition-colors hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
         >
           <Check size={24} weight="bold" />
           Confirmar
